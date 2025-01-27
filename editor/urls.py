22e45574095dd716a9bf71121e7ebbf2ede96b07
tolelom <98kimsungmin@naver.com>
2025-01-27 21:50:50 +0900

    - 에디터 초안 완성

--- editor/urls.py
+++ editor/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import editor_view
+from .views import editor_view, execute_code
 
 app_name = 'editor'
 urlpatterns = [
     path('', editor_view, name='home'),
+    path('execute_code/', execute_code, name='execute_code'),
 ]
